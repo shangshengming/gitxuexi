3f16c5a377db63cab56d385433084c18f1143b1c
shangshengming <shangshengming@gmail.com>
2021-11-13 23:15:51 +0900

    第一次提交文件

---
+++ src/main/java/com/example/diyige/DiyigeApplication.java
@@ -0,0 +1,13 @@
+package com.example.diyige;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiyigeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiyigeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/diyige/DiyigeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diyige;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiyigeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
